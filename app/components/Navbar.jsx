199cd48d856649b3a17adda3aebdf57386127ec9
Fran Agulto <francis.agulto@wpengine.com>
2023-09-26 10:52:01 -0500

    added middleware

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "./cloudinary-logo.png";
-export default function Navbar() {
+import LogoutButton from "./LogoutButton";
+export default function Navbar({ user }) {
   return (
     <nav>
       <Image
         src={Logo}
-        alt="Cloudinary Logo"
+        alt="Dojo Helpdesk logo"
         width={70}
         placeholder="blur"
         quality={100}
       />
-      <h1>Cloudinary Gallery</h1>
+      <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
+      {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   );
 }
